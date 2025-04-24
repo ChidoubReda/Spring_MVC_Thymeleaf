b906dc75c835c897b970e0a327d271f5a0e2cb4b
ChidoubReda <Reda.Chidoub@emsi-edu.ma>
2025-04-24 10:24:31 +0200

    First Commit

---
+++ src/main/java/com/springdemo/spring_mvc_thymeleaf_0/SpringMvcThymeleaf0Application.java
@@ -0,0 +1,13 @@
+package com.springdemo.spring_mvc_thymeleaf_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcThymeleaf0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcThymeleaf0Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/springdemo/spring_mvc_thymeleaf_0/SpringMvcThymeleaf0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.spring_mvc_thymeleaf_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcThymeleaf0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
